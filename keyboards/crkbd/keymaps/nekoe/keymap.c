bd66bc6a0ce0ee3551313ce68069eb2f1e79268d
nekoe <kazuhiro.nakae@gmail.com>
2020-09-10 15:12:48 +0900

    tab_opt and swap thumb keys

--- keyboards/crkbd/keymaps/nekoe/keymap.c
+++ keyboards/crkbd/keymaps/nekoe/keymap.c
@@ -42,8 +42,9 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 };
 
 // custom macros
-#define SPC_L2  LT(2, KC_SPC)    // tap: spc,     hold: layer2
-#define ENT_L2  LT(2, KC_ENT)    // tap: ent,     hold: layer2
+#define SPC_L2  LT(2, KC_SPC)        // tap: spc, hold: layer2
+#define ENT_L2  LT(2, KC_ENT)        // tap: ent, hold: layer2
+#define TAB_OPT MT(MOD_LALT, KC_TAB) // tap: tab, hold: opt
 
 #define ____    KC_NO
 #define T___    KC_TRANSPARENT
@@ -57,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_MINS,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
+      TAB_OPT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,    LANG,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_RALT,  SPC_L2,   MO(1),    KC_LGUI,  ENT_L2, KC_RCTL \
+                                          KC_LCTL,   MO(1),  SPC_L2,     ENT_L2, KC_LGUI,    LANG \
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -70,11 +71,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB,    T___,    T___,    T___,    T___,    T___,                         T___,    T___,    T___,    T___,    T___, KC_LBRC,\
+      TAB_OPT,    T___,    T___,    T___,    T___,    T___,                         T___,    T___,    T___,    T___,    T___, KC_LBRC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    T___,    T___,    T___,    T___,    T___,                         T___,    T___,    T___,    T___, KC_BSLS, KC_RBRC,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_RALT,  SPC_L2,   MO(1),    KC_LGUI,  ENT_L2, KC_RCTL \
+                                          KC_LCTL,   MO(1),  SPC_L2,     ENT_L2, KC_LGUI,    LANG \
                                       //`--------------------------'  `--------------------------'
     ),
 
@@ -86,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,  CMD(Z),  CMD(X),  CMD(C),  CMD(V), KC_LEFT,                      KC_DOWN,    ____,    ____,    ____,    ____,    ____,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_RALT,  SPC_L2,   MO(1),    KC_LGUI,  ENT_L2, KC_RCTL \
+                                          KC_LCTL,   MO(1),  SPC_L2,   CMD(ENT), KC_LGUI,    LANG \
                                       //`--------------------------'  `--------------------------'
   )
 };
