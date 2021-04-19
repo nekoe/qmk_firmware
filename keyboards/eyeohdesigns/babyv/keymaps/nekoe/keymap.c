cdbde950de82b85f18d1cf796428770165d2be6d
nekoe <kazuhiro.nakae@gmail.com>
2021-04-19 11:15:53 +0900

    fix layer number

--- keyboards/eyeohdesigns/babyv/keymaps/nekoe/keymap.c
+++ keyboards/eyeohdesigns/babyv/keymaps/nekoe/keymap.c
@@ -15,14 +15,6 @@
  */
 #include QMK_KEYBOARD_H
 
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-   
-    QWERTY,
-    FUNCTN,
-    NUMBRS,
-};
-
 // lang
 #define LANG_EN  KC_LANG2
 #define LANG_JP  KC_LANG1
@@ -43,25 +35,25 @@ enum layer_names {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-   [QWERTY] = LAYOUT_1u(
+   [0] = LAYOUT_1u(
         KC_ESC,   KC_Q,  KC_W,  KC_E,     KC_R,   KC_T,       KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_MINS,
         TAB_OPT,  KC_A,  KC_S,  KC_D,     KC_F,   KC_G,       KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
         KC_LSFT,  KC_Z,  KC_X,  KC_C,     KC_V,   KC_B,       KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  ____,
         L3,                     KC_LCTL,  EN_L1,  SPC_L2,     ENT_L2,  JP_CMD,  ____,                        ____
     ),
-    [NUMBRS] = LAYOUT_1u(
+    [1] = LAYOUT_1u(
         KC_GRV,   KC_1,  KC_2,  KC_3,     KC_4,   KC_5,       KC_6,    KC_7,    KC_8,  KC_9,  KC_0,     KC_EQL,
         TAB_OPT,  T___,  T___,  T___,     T___,   T___,       T___,    T___,    T___,  T___,  T___,     KC_LBRC,
         KC_LSFT,  T___,  T___,  T___,     T___,   T___,       T___,    T___,    T___,  T___,  KC_BSLS,  KC_RBRC,
         ____,                   KC_LCTL,  EN_L1,  SPC_L2,     ENT_L2,  JP_CMD,  ____,                   ____
     ),
-    [FUNCTN] = LAYOUT_1u(
+    [2] = LAYOUT_1u(
         ____,     ____,    ____,    CTL(E),   ____,     ____,        ____,      ____,    ____,    ____,  KC_UP,  ____,\
         ____,     CTL(A),  CMD(S),  KC_DEL,   KC_RGHT,  ____,        KC_BSPC,   ____,    CTL(K),  ____,  ____,   ____,\
         KC_LSFT,  CMD(Z),  CMD(X),  CMD(C),   CMD(V),   KC_LEFT,     KC_DOWN,   ____,    ____,    ____,  ____,   ____,\
         ____,                       KC_LCTL,  EN_L1,    SPC_L2,      CMD(ENT),  JP_CMD,  ____,                   ____
     ),
-    [3]      = LAYOUT_1u(
+    [3] = LAYOUT_1u(
         ____,  ____,  ____,  ____,  ____,  ____,     ____,  ____,  ____,  ____,  ____,  ____,\
         ____,  ____,  ____,  ____,  ____,  ____,     ____,  ____,  ____,  ____,  ____,  ____,\
         ____,  ____,  ____,  ____,  ____,  ____,     ____,  ____,  ____,  ____,  ____,  ____,\
