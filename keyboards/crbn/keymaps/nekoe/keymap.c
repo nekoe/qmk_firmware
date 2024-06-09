36570da380e2aea6dd058dfa97a6cf14fc18cb0a
nekoe <kazuhiro.nakae@gmail.com>
2024-06-09 16:38:42 +0900

    update crbn

--- keyboards/crbn/keymaps/nekoe/keymap.c
+++ keyboards/crbn/keymaps/nekoe/keymap.c
@@ -36,24 +36,24 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[0] = { 
-		{ KC_ESC,   KC_Q,  KC_W,  KC_E,  KC_R,   KC_T,      KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_MINS },
-		{ TAB_OPT,  KC_A,  KC_S,  KC_D,  KC_F,   KC_G,      KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT },
-		{ KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,   KC_B,      KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  ____    },
-		{ KC_LCTL,  ____,  ____,  ____,  EN_L1,  SPC_L2,    ENT_L2,  JP_CMD,  ____,     ____,    ____,     ____    }
+		{ KC_ESC,   KC_Q,  KC_W,  KC_E,     KC_R,   KC_T,      KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_MINS },
+		{ TAB_OPT,  KC_A,  KC_S,  KC_D,     KC_F,   KC_G,      KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT },
+		{ KC_LSFT,  KC_Z,  KC_X,  KC_C,     KC_V,   KC_B,      KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  ____    },
+		{ ____,     ____,  ____,  KC_LCTL,  EN_L1,  SPC_L2,    ENT_L2,  JP_CMD,  ____,     ____,    ____,     ____    }
 	},
 
 	[1] = { 
-		{ KC_GRV,   KC_1,  KC_2,  KC_3,  KC_4,   KC_5,      KC_6,    KC_7,    KC_8,     KC_9,    KC_0,     KC_EQL  },
-		{ TAB_OPT,  T___,  T___,  T___,  T___,   T___,      T___,    T___,    T___,     T___,    T___,     KC_LBRC },
-		{ KC_LSFT,  T___,  T___,  T___,  T___,   T___,      T___,    T___,    T___,     T___,    KC_BSLS,  KC_RBRC },
-		{ KC_LCTL,  ____,  ____,  ____,  EN_L1,  SPC_L2,    ENT_L2,  JP_CMD,  ____,     ____,    ____,     ____    }
+		{ KC_GRV,   KC_1,  KC_2,  KC_3,     KC_4,   KC_5,      KC_6,    KC_7,    KC_8,     KC_9,    KC_0,     KC_EQL  },
+		{ TAB_OPT,  T___,  T___,  T___,     T___,   T___,      T___,    T___,    T___,     T___,    T___,     KC_LBRC },
+		{ KC_LSFT,  T___,  T___,  T___,     T___,   T___,      T___,    T___,    T___,     T___,    KC_BSLS,  KC_RBRC },
+		{ ____,     ____,  ____,  KC_LCTL,  EN_L1,  SPC_L2,    ENT_L2,  JP_CMD,  ____,     ____,    ____,     ____    }
 	},
 
 	[2] = { 
-		{ ____,     ____,    ____,    CTL(E),  ____,     ____,        ____,      ____,    ____,    ____,  KC_UP,  ____ },
-		{ ____,     CTL(A),  CMD(S),  KC_DEL,  KC_RGHT,  ____,        KC_BSPC,   ____,    CTL(K),  ____,  ____,   ____ },
-		{ KC_LSFT,  CMD(Z),  CMD(X),  CMD(C),  CMD(V),   KC_LEFT,     KC_DOWN,   ____,    ____,    ____,  ____,   ____ },
-		{ KC_LCTL,  ____,    ____,    ____,    EN_L1,    SPC_L2,      CMD(ENT),  JP_CMD,  ____,    ____,  ____,   QK_BOOT }
+		{ ____,     ____,    ____,    CTL(E),   ____,     ____,        ____,      ____,    ____,    ____,  KC_UP,  ____ },
+		{ ____,     CTL(A),  CMD(S),  KC_DEL,   KC_RGHT,  ____,        KC_BSPC,   ____,    CTL(K),  ____,  ____,   ____ },
+		{ KC_LSFT,  CMD(Z),  CMD(X),  CMD(C),   CMD(V),   KC_LEFT,     KC_DOWN,   ____,    ____,    ____,  ____,   ____ },
+		{ ____,     ____,    ____,    KC_LCTL,  EN_L1,    SPC_L2,      CMD(ENT),  JP_CMD,  ____,    ____,  ____,   QK_BOOT }
 	}
 
 };
